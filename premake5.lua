9019453306b6f57d80336e7eb2518b5f935f45d5
Rydeen777 <zsbt2012-5@yahoo.co.jp>
2021-08-27 09:55:59 +0900

    add premake5.lua and modify .gitignore

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imgui_rectpack.h",
+        "imgui_textedit.h",
+        "imgui_truetype.h",
+        "imgui_demo.cpp",
+        "imgui_tables.cpp"
+	}
+		
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
