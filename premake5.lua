ef47ef31b36f6ae4de4302711b543c57409c158f
Rydeen777 <zsbt2012-5@yahoo.co.jp>
2021-08-31 21:18:12 +0900

    change premake file

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
-	language "C"
-	staticruntime "off"
+	language "C++"
+    cppdialect "C++17"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -26,7 +27,6 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		--staticruntime "On"
 
 
 	filter "configurations:Debug"
